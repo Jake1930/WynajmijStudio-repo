783c42033cf651ddf26504834254f759a8636fe2
Jake1930 <kubaprzyciasa@gmail.com>
2023-11-21 23:59:03 +0100

    Update script file

---
+++ WynajmijStudio/script.js
@@ -0,0 +1,16 @@
+let background = document.getElementById('parallax-background');
+
+window.addEventListener('scroll', function(){
+    let value = window.scrollY;
+    background.style.top = value * 0.45 + 'px';
+})
+
+const preloader = document.querySelector('#preloader');
+
+preloader.classList.add('show-preloader');
+
+window.addEventListener('load', function () {
+    setTimeout(function(){
+        preloader.classList.remove('show-preloader');
+    }, 1000);
+});
