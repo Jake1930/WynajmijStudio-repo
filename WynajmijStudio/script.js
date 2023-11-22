45cc03d72244182fbe3b9e631a887e83d70b8ad5
Jake1930 <kubaprzyciasa@gmail.com>
2023-11-22 20:19:38 +0100

    preloader

--- WynajmijStudio/script.js
+++ WynajmijStudio/script.js
@@ -12,5 +12,5 @@ preloader.classList.add('show-preloader');
 window.addEventListener('load', function () {
     setTimeout(function(){
         preloader.classList.remove('show-preloader');
-    }, 1000);
+    }, 200);
 });
